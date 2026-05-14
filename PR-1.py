fd3b7659fa30837880b1043166c10ba22652a44d
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-05-14 18:25:37 +0530

    Update PR-1.py

--- PR-1.py
+++ PR-1.py
@@ -7,7 +7,7 @@ favorite_place = input("Please enter your favorite place: ")
 favorite_number = input("Please enter your favorite number: ")
 favorite_food = input("Please enter your favorite food: ")
 favorite_ipl_team = input("Please enter your favorite IPL team: ")
-hobbies = input("Please enter your hobbies : ")
+hobbies = input("Please enter your hobbies : ","\n")
 print("Thank you for providing your information! Here is what we have collected:","\n")
 print("Name: ", name,"type: ", type(name), "memory address: ", id(name))
 print("Age: ", age,"type: ", type(age), "memory address: ", id(age))
@@ -23,6 +23,7 @@ print("thank you for using the interactive personal data collector! Have a great
 '''
 output:
 welcome to the interactive personal data collector! 
+
 Please enter your name: neev
 Please enter your age: 18
 Please enter your height in cm: 1.80
@@ -32,7 +33,9 @@ Please enter your favorite number: 77
 Please enter your favorite food: pizza
 Please enter your favorite IPL team: GT
 Please enter your hobbies : Skydiving
+
 Thank you for providing your information! Here is what we have collected:
+
 Name:  neev type:  <class 'str'> memory address:  2006822627552
 Age:  18 type:  <class 'int'> memory address:  2006822627600
 Height:  1.8 cm type:  <class 'float'> memory address:  2006822627648
@@ -43,5 +46,6 @@ Favorite Food:  pizza type:  <class 'str'> memory address:  2006822627840
 Hobbies:  Skydiving type:  <class 'str'> memory address:  2006822682096
 Favorite IPL Team:  GT type:  <class 'str'> memory address:  2006822627888
 Your birth year is:  2007  based on your agre of  18
+
 thank you for using the interactive personal data collector! Have a great day!
 '''
